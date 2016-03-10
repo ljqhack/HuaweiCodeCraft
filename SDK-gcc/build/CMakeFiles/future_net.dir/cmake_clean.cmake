ebdb955f962388f46025dce633d970da7dd9db02
ljqhack <ljqhack@qq.com>
2016-03-10 10:08:37 +0800

    init commit

---
+++ SDK-gcc/build/CMakeFiles/future_net.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/future_net.dir/future_net.cpp.o"
+  "CMakeFiles/future_net.dir/io.cpp.o"
+  "CMakeFiles/future_net.dir/route.cpp.o"
+  "/home/ljqhack/downloads/hua/SDK-gcc/bin/future_net.pdb"
+  "/home/ljqhack/downloads/hua/SDK-gcc/bin/future_net"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/future_net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
