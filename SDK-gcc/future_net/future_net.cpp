523e999dc9473f8b9ef207f3ecb4fe1a812f918c
ljqhack <ljqhack@qq.com>
2016-03-13 19:16:33 +0800

    build_graph

--- SDK-gcc/future_net/future_net.cpp
+++ SDK-gcc/future_net/future_net.cpp
@@ -2,23 +2,37 @@
 #include "lib_io.h"
 #include "lib_time.h"
 #include "stdio.h"
+#include "string.h"
+
+#define TF  "../test-case/case1/topo.csv"
+#define DF  "../test-case/case1/demand.csv"
+#define RF  "../test-case/case1/sample_result.csv"
 
 int main(int argc, char *argv[])
 {
+    printf("%d  %d", '\n', '\r');
     print_time("Begin");
     char *topo[5000];
     int edge_num;
     char *demand;
     int demand_num;
 
-    char *topo_file = argv[1];
+    //char *topo_file = argv[1];
+    const char *topo_file = TF;
     edge_num = read_file(topo, 5000, topo_file);
     if (edge_num == 0)
     {
         printf("Please input valid topo file.\n");
         return -1;
     }
-    char *demand_file = argv[2];
+    printf("edge_num:%d\n", edge_num);
+    int i = 0;
+    while(i < edge_num)
+    {
+        printf("topo0:%s\n",topo[i++]);
+    }
+    //char *demand_file = argv[2];
+    const char *demand_file = DF;
     demand_num = read_file(&demand, 1, demand_file);
     if (demand_num != 1)
     {
@@ -28,7 +42,8 @@ int main(int argc, char *argv[])
 
     search_route(topo, edge_num, demand);
 
-    char *result_file = argv[3];
+    //char *result_file = argv[3];
+    const char *result_file = RF;
     write_result(result_file);
     release_buff(topo, edge_num);
     release_buff(&demand, 1);
--- SDK-gcc/future_net/route.cpp
+++ SDK-gcc/future_net/route.cpp
@@ -2,12 +2,124 @@
 #include "route.h"
 #include "lib_record.h"
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#define MAX_NODE 600
+#define MAX_LINK 4800
+typedef struct ArcBox{
+    int tailvex, headvex;
+    struct ArcBox *tlink, *hlink;
+    int cost;
+    int id;
+}ArcBox;
+
+typedef struct VexNode{
+    int id;
+    ArcBox *firstin, *firstout;
+}VexNode;
+
+typedef struct{
+    VexNode node[MAX_NODE];
+    int vexnum, arcnum;
+}Graph;
+
+Graph G;
+ArcBox Link[MAX_LINK];
+
+int str_to_int(char *topo, int &lid, int &sid, int &did, int &cost)
+{
+    int i = 0, j, num, ws;
+    char *base = topo;
+    for(i=0; i < 4; i++)
+    {
+        num = 0;
+        if(i < 3)
+        {
+            while( (*topo++) != ',');
+            ws = topo - base - 1;
+        }
+        else
+        {
+            while( (*topo) != '\0' && (*topo) != '\n' )
+                topo++;
+             ws = topo - base;
+        }
+        for(j = 0; j < ws; j++)
+            num += (base[j] - 0x30) * pow(10, ws - j - 1);
+        if(0 == i)
+            lid = num;
+        else if(1 == i)
+            sid = num;
+        else if(2 == i)
+            did = num;
+        else if(3 == i)
+            cost = num;
+        base = topo;
+    }
+    return 1;
+}
+
+void build_graph(int lid, int sid, int did, int cost)
+{
+	ArcBox *l;
+    Link[lid].id = lid;
+    Link[lid].tailvex = sid;
+	Link[lid].headvex = did;
+	Link[lid].cost = cost;
+	Link[lid].tlink = NULL;
+	Link[lid].hlink = NULL;
+	G.node[sid].id = sid;
+	G.node[did].id = did;
+	
+	if(G.node[sid].firstout)
+	{
+		l = G.node[sid].firstout;
+		while(l->tlink)
+		{
+			l = l->tlink;
+		}
+		l->tlink = &Link[lid];
+	}
+	else
+	{
+		G.node[sid].firstout = &Link[lid];
+	}
+	
+	if(G.node[did].firstin)
+	{
+		l = G.node[did].firstin;
+		while(l->hlink)
+		{
+			l = l->hlink;
+		}
+		l->hlink = &Link[lid];
+	}
+	else
+	{
+		G.node[did].firstin = &Link[lid];
+	}
+	
+}
 
 //你要完成的功能总入口
 void search_route(char *topo[5000], int edge_num, char *demand)
 {
+    /*
     unsigned short result[] = {2, 6, 3};//示例中的一个解
 
     for (int i = 0; i < 3; i++)
         record_result(result[i]);
+    */
+    printf("search_route\n");
+    int i;
+    int lid, sid, did, cost;
+    G.arcnum = edge_num;
+    for(i = 0; i < edge_num; i++)
+    {
+        str_to_int(topo[i], lid, sid, did, cost);
+        printf("read info from topo[%d]:%d  %d  %d  %d \n", i, lid, sid, did, cost);
+        build_graph(lid, sid, did, cost);
+    }
+    printf("end_search_route\n");
 }
